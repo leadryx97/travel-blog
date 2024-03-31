2b8d6f15036bc7364371f86c7fc2e202e3546ebf
Teldryxia <leadryx@gmail.com>
2024-03-31 20:35:56 +0200

    updated file

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,10 +1,11 @@
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
+import './styles/main.scss';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Reiseblog",
-  description: "Verfolge unsere Abenteuer",
+	title: 'Reiseblog',
+	description: 'Verfolge unsere Abenteuer',
 };
 
 /* next.js initial code
@@ -18,9 +19,9 @@ export default function RootLayout({ children }) {
 */
 
 const rootLayout = ({ children }) => (
-  <html lang="de-CH">
-    <body className={inter.className}>{children}</body>
-  </html>
+	<html lang="de-CH">
+		<body className={inter.className}>{children}</body>
+	</html>
 );
 
 export default rootLayout;
