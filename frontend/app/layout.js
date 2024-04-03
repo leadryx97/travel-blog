1de4cb0fa6886dfb6754906a75acda7941fe7da6
Teldryxia <leadryx@gmail.com>
2024-04-03 14:19:52 +0200

    added header

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from 'next/font/google';
 import './styles/main.scss';
 
+// import header
+import Header from './components/header/Header';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -11,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="de-CH">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Header />
+				{children}
+			</body>
 		</html>
 	);
 }
