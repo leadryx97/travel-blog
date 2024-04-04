d0a01fb627238bc5bef60d52771fcefeee70a48c
Teldryxia <leadryx@gmail.com>
2024-04-04 17:02:19 +0200

    imported fonts

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,10 +1,11 @@
-import { Inter } from 'next/font/google';
+import { Roboto_Flex, Playfair_Display } from 'next/font/google';
 import './styles/main.scss';
 
 // import header
 import Header from './components/header/Header';
 
-const inter = Inter({ subsets: ['latin'] });
+const robotoFlex = Roboto_Flex({ subsets: ['latin'] });
+const playfairDisplay = Playfair_Display({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'Reiseblog',
@@ -14,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="de-CH">
-			<body className={inter.className}>
+			<body className={robotoFlex.className}>
 				<Header />
 				{children}
 			</body>
