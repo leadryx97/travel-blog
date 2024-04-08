b372e6a75ab289bb7b7ed0650a14ab9fd5c1d277
Teldryxia <leadryx@gmail.com>
2024-04-08 22:59:52 +0200

    changed google font import

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,17 +1,20 @@
-import { Roboto_Flex, Playfair_Display } from 'next/font/google';
+// import main scss file
 import './styles/main.scss';
-
+// import google font
+import { Roboto_Flex } from 'next/font/google';
 // import header
 import Header from './components/header/Header';
 
-const robotoFlex = Roboto_Flex({ subsets: ['latin'] });
-const playfairDisplay = Playfair_Display({ subsets: ['latin'] });
+// declare font variable
+export const robotoFlex = Roboto_Flex({ subsets: ['latin'] });
 
+// declare metadata
 export const metadata = {
 	title: 'Reiseblog',
 	description: 'Verfolge unsere Abenteuer',
 };
 
+// root layout function
 export default function RootLayout({ children }) {
 	return (
 		<html lang="de-CH">
