96ae234b07c950300cf6a02cc9e200e477db145d
Teldryxia <leadryx@gmail.com>
2024-04-12 20:20:57 +0200

    extended blog post content

--- frontend/app/components/blogPost/BlogPost.js
+++ frontend/app/components/blogPost/BlogPost.js
@@ -1,17 +1,27 @@
 // import next.js image component
 import Image from 'next/image';
 
+// import text block component
+import TextBlock from './TextBlock';
+
 // define blog post component
 // extract slug property with params argument
 export default async function BlogPost({ params }) {
 	const { slug } = params;
 	// fetch blog post data from strapi api
 	const response = await fetch(`
-	http://localhost:1337/api/posts/?[filters][slug]=${slug}&populate[0]=CoverImage&populate[1]=CoverImage.url`);
+	http://localhost:1337/api/posts/?[filters][slug]=${slug}&populate[0]=CoverImage&populate[1]=CoverImage.url&populate=Blocks&populate=Blocks.id`);
 	// parse json data from the response body
 	const postData = await response.json();
 	// variable for post data object
 	const post = postData.data;
+
+	const blocks = post[0].attributes.Blocks;
+	console.log('Blocks?', blocks);
+
+	blocks.forEach((block) => {
+		console.log('Component:', block.__component);
+	});
 
 	// variable for cover image
 	const coverImage = post[0].attributes.CoverImage;
@@ -39,6 +49,23 @@ export default async function BlogPost({ params }) {
 					<p>{item.attributes.Date}</p>
 					<p>{item.attributes.Intro}</p>
 					<p>{item.attributes.Content}</p>
+
+					{/* Iterate over the dynamicZone blocks */}
+					{item.attributes.Blocks.map((block, index) => {
+						// Switch based on the __component value of each block
+						switch (block.__component) {
+							case 'post.image-slider':
+								return <div />;
+							// if there is a text block in the dynamic zone, render TextBlock component
+							case 'post.text':
+								return <TextBlock text={block.Text} />;
+							case 'post.single-image':
+								return <div />;
+							// Add cases for other components as needed
+							default:
+								return null;
+						}
+					})}
 				</div>
 			))}
 		</div>
