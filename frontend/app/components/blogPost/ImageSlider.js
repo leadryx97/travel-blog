62162f5a04410fcfdc78e512e9b9f6d43f852a43
Teldryxia <leadryx@gmail.com>
2024-04-13 16:43:22 +0200

    created image slider component

---
+++ frontend/app/components/blogPost/ImageSlider.js
@@ -0,0 +1,31 @@
+// import next.js image component
+import Image from 'next/image';
+
+export function ImageItem({ imgSrc, imgAlt, imgCaption }) {
+	return (
+		<figure>
+			<Image src={imgSrc} alt={imgAlt} width="500" height="400" />
+			<figcaption>{imgCaption}</figcaption>
+		</figure>
+	);
+}
+
+export default function ImageSlider({ images }) {
+	return (
+		<div className="imageSlider">
+			{images.map(
+				(image, index) => (
+					console.log('IMG URL CHECK:', image.attributes.url),
+					(
+						<ImageItem
+							key={index}
+							imgSrc={'http://localhost:1337' + image.attributes.url}
+							imgAlt={image.attributes.alternativeText}
+							imgCaption={image.attributes.caption}
+						/>
+					)
+				)
+			)}
+		</div>
+	);
+}
