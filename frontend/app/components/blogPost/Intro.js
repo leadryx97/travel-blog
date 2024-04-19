60c58f46fe2143b1ecee6e2070766405cc391586
Teldryxia <leadryx@gmail.com>
2024-04-19 17:41:46 +0200

    adjusted intro

--- frontend/app/components/blogPost/Intro.js
+++ frontend/app/components/blogPost/Intro.js
@@ -1,12 +1,19 @@
 // import styles
-import styles from '../../styles/components/Intro.module.scss';
+import styles from '../../styles/components/blogPost/Intro.module.scss';
 
-export default function Intro({ title, date, intro }) {
+export default function Intro({ title, date, day, intro }) {
+	// split the date string into year, month, and day
+	const [year, month, dayOfMonth] = date.split('-');
+	// concatenate them in the desired format
+	const formattedDate = `${dayOfMonth}.${month}.${year}`;
 	return (
-		<div className={styles.introContainer}>
+		<div className={styles.intro}>
 			<h1 className={`${styles.h1} ${styles['h1--blogPost']}`}>{title}</h1>
-			<p>{date}</p>
-			<p>{intro}</p>
+			<div className={styles.intro__dateContainer}>
+				<p>{formattedDate}</p>
+				<p>{day}</p>
+			</div>
+			<p className={styles.intro__text}>{intro}</p>
 		</div>
 	);
 }
