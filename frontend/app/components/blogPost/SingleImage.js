8db4a7d3d7e86714376d97c414010fe8a8a5d314
Teldryxia <leadryx@gmail.com>
2024-04-19 12:09:36 +0200

    exchanged Image component

--- frontend/app/components/blogPost/SingleImage.js
+++ frontend/app/components/blogPost/SingleImage.js
@@ -2,9 +2,11 @@
 import Image from 'next/image';
 
 export default function SingleImage({ imgSrc, imgAlt, imgCaption }) {
+	// Image component not working in dev
+	// <Image src={imgSrc} alt={imgAlt} width="500" height="400" />
 	return (
 		<figure>
-			<Image src={imgSrc} alt={imgAlt} width="500" height="400" />
+			<img src={imgSrc} alt={imgAlt} width="500" height="400" />
 			<figcaption>{imgCaption}</figcaption>
 		</figure>
 	);
