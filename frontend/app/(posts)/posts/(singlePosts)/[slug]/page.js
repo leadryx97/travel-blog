d7c046065776899ff1754185cb210fd1ce8d13b9
Teldryxia <leadryx@gmail.com>
2024-04-19 12:11:56 +0200

    adjusted fetch url

--- frontend/app/(posts)/posts/(singlePosts)/[slug]/page.js
+++ frontend/app/(posts)/posts/(singlePosts)/[slug]/page.js
@@ -7,7 +7,6 @@ export async function generateStaticParams() {
 	const response = await fetch('http://127.0.0.1:1337/api/posts');
 	// parse json data from the response body
 	const postData = await response.json();
-	console.log('PostData:', postData);
 	/* maps over the array of blog post data
 	returns an array of objects, each containing a slug property
 	The slug is extracted from each blog post's attributes and converted to a string */
@@ -19,7 +18,6 @@ export async function generateStaticParams() {
 export default function BlogPostPage({ params }) {
 	// slug is extracted from params object
 	const slug = params;
-	console.log('Slug page.js:', slug);
 	// renders component BlogPost and passes slug as a prop
 	return <BlogPost params={slug} />;
 }
