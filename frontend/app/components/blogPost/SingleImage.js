c6b89657619c98d48b8f32b0b80400bba23f79cc
Teldryxia <leadryx@gmail.com>
2024-04-20 10:08:58 +0200

    adjusted content

--- frontend/app/components/blogPost/SingleImage.js
+++ frontend/app/components/blogPost/SingleImage.js
@@ -1,13 +1,17 @@
 // import next.js image component
 import Image from 'next/image';
+// import styles
+import styles from '../../styles/components/blogPost/SingleImage.module.scss';
 
 export default function SingleImage({ imgSrc, imgAlt, imgCaption }) {
 	// Image component not working in dev
 	// <Image src={imgSrc} alt={imgAlt} width="500" height="400" />
 	return (
-		<figure>
-			<img src={imgSrc} alt={imgAlt} width="300" height="200" />
-			<figcaption>{imgCaption}</figcaption>
+		<figure className={styles.singleImg}>
+			<img src={imgSrc} alt={imgAlt} className={styles.singleImg__item} />
+			<figcaption>
+				<p className={styles.singleImg__caption}>{imgCaption}</p>
+			</figcaption>
 		</figure>
 	);
 }
