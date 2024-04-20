9f05d1ac04b737e40319d025511b87bf87f55a16
Teldryxia <leadryx@gmail.com>
2024-04-20 10:09:21 +0200

    added date formatter

--- frontend/app/components/blogPost/Intro.js
+++ frontend/app/components/blogPost/Intro.js
@@ -10,10 +10,10 @@ export default function Intro({ title, date, day, intro }) {
 		<div className={styles.intro}>
 			<h1 className={`${styles.h1} ${styles['h1--blogPost']}`}>{title}</h1>
 			<div className={styles.intro__dateContainer}>
-				<p>{formattedDate}</p>
-				<p>{day}</p>
+				<p className={styles.p}>{formattedDate}</p>
+				<p className={styles.p}>{day}</p>
 			</div>
-			<p className={styles.intro__text}>{intro}</p>
+			<p className={`${styles.intro__text} ${styles.p}`}>{intro}</p>
 		</div>
 	);
 }
