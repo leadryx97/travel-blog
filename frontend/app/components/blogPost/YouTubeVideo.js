cf384f0550dbe074947c9dc10fa42ddf2e1b2469
Teldryxia <leadryx@gmail.com>
2024-04-24 23:34:04 +0800

    allowed full screen

--- frontend/app/components/blogPost/YouTubeVideo.js
+++ frontend/app/components/blogPost/YouTubeVideo.js
@@ -4,7 +4,14 @@ import styles from '../../styles/components/blogPost/YouTubeVideo.module.scss';
 export default function YouTubeVideo({ url, title }) {
 	return (
 		<div className={styles.youTubeContainer}>
-			<iframe title={title} src={url} className={styles.youtubeVideo} />;
+			<iframe
+				title={title}
+				src={url}
+				className={styles.youtubeVideo}
+				allowfullscreen
+				allow="fullscreen;"
+			/>
+			;
 		</div>
 	);
 }
