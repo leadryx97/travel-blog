1277dc070b78d81790cbfa3c4789c269aeaa2414
Teldryxia <leadryx@gmail.com>
2024-04-24 13:47:01 +0800

    changed domain

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
 	// allow next.js to load images from domain
 	images: {
-		domains: ['localhost'],
+		domains: ['https://peaceful-citadel-90180-369fa539b5ab.herokuapp.com'],
 	},
 };
 
