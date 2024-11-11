f314187e4690566d631bf24710ae66c21199da58
Teldryxia <leadryx@gmail.com>
2024-11-11 17:10:56 +0100

    added intersection observer for animation

--- frontend/app/components/blogPost/CustomBlocksRenderer.js
+++ frontend/app/components/blogPost/CustomBlocksRenderer.js
@@ -3,14 +3,55 @@
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
 // import main scss file
 import '../../styles/globals.scss';
+// import styles
+import styles from '../../styles/components/blogPost/CustomBlocks.module.scss';
+// import react hooks
+import React, { useRef, useState, useEffect } from 'react';
 
-// Render the BlocksRenderer with the content from the post
+// render the BlocksRenderer with the content from the post
 export default function CustomBlocksRenderer({ content }) {
+	// defining visibility state for triggering animation on scroll
+	const [isVisible, setIsVisible] = useState(false);
+	// defining reference object for the element we want to observe
+	const elementRef = useRef(null);
+
+	// useEffect hook to set up intersection observer for scroll-triggered animations
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				// check if observed element is visible in the viewport
+				if (entry.isIntersecting) {
+					setIsVisible(true); // update state to make element visible
+					observer.unobserve(elementRef.current); // stop observing after animation is triggered
+				}
+			},
+			{ threshold: 0.2 } // trigger animation when 20% of element is visible
+		);
+		// start observing the element when component mounts
+		if (elementRef.current) {
+			observer.observe(elementRef.current);
+		}
+		// stop observing the element on component unmount or re-render
+		return () => {
+			if (elementRef.current) {
+				observer.unobserve(elementRef.current);
+			}
+		};
+	}, []); // empty dependency array ensures effect only runs once after initial render
 	return (
 		<BlocksRenderer
 			content={content}
 			blocks={{
-				paragraph: ({ children }) => <p className="p">{children}</p>,
+				paragraph: ({ children }) => (
+					<p
+						ref={elementRef} // attaches elementRef to observe it
+						className={`p ${styles.txt} ${
+							isVisible ? styles['txt--visible'] : ''
+						}`}
+					>
+						{children}
+					</p>
+				),
 				heading: ({ children, level }) => {
 					switch (level) {
 						case 1:
